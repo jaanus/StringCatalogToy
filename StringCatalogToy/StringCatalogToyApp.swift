fc59f534a87bca4753822922e1dc93a9f0847d6c
Jaanus Kase <jaanus@gmail.com>
2024-04-08 11:12:48 +0300

    Initial Commit

---
+++ StringCatalogToy/StringCatalogToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCatalogToyApp.swift
+//  StringCatalogToy
+//
+//  Created by Jaanus Kase on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct StringCatalogToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
